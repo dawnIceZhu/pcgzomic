4877b30d2adc08a3f5c2e1f5f52063038bf7ff0a
FengZhenYu <fengzhenyu9999@163.com>
2021-03-30 11:22:22 +0800

    feat: 学员作品

--- src/components/StudentWorks.vue
+++ src/components/StudentWorks.vue
@@ -37,16 +37,105 @@
             </div>
           </a-tab-pane>
           <a-tab-pane key="2" tab="西餐料理 /Western Cuisine" force-render>
-            Content of Tab Pane 2
+            <div class="pane-content">
+              <div class="pane-left">
+                <router-link to="">
+                  <img src="http://www.gzomick.com/images/works/xc/xc1.jpg" alt="">
+                </router-link>
+              </div>
+              <div class="pane-right">
+                <div>
+                  <router-link to=""><img src="http://www.gzomick.com/images/works/xc/xc2.jpg" alt=""></router-link>
+                </div>
+                <div>
+                  <router-link to=""><img src="http://www.gzomick.com/images/works/xc/xc3.jpg" alt=""></router-link>
+                </div>
+                <div>
+                  <router-link to=""><img src="http://www.gzomick.com/images/works/xc/xc4.jpg" alt=""></router-link>
+                </div>
+                <div>
+                  <router-link to=""><img src="http://www.gzomick.com/images/works/xc/xc5.jpg" alt=""></router-link>
+                </div>
+                <div>
+                  <router-link to=""><img src="http://www.gzomick.com/images/works/xc/xc6.jpg" alt=""></router-link>
+                </div>
+                <div>
+                  <router-link to=""><img src="http://www.gzomick.com/images/works/xc/xc7.jpg" alt=""></router-link>
+                </div>
+              </div>
+            </div>
           </a-tab-pane>
           <a-tab-pane key="3" tab="精品调酒 /Mixology">
-            Content of Tab Pane 3
+            <div class="pane-content">
+              <div class="pane-left">
+                <router-link to="">
+                  <img src="http://www.gzomick.com/statics/images/index/tj_food1.jpg" alt="">
+                </router-link>
+              </div>
+              <div class="pane-right">
+                <div>
+                  <router-link to=""><img src="http://www.gzomick.com/statics/images/index/tj_food2.jpg" alt=""></router-link>
+                </div>
+                <div>
+                  <router-link to=""><img src="http://www.gzomick.com/statics/images/index/tj_food3.jpg" alt=""></router-link>
+                </div>
+                <div>
+                  <router-link to=""><img src="http://www.gzomick.com/statics/images/index/tj_food4.jpg" alt=""></router-link>
+                </div>
+                <div>
+                  <router-link to=""><img src="http://www.gzomick.com/statics/images/index/tj_food5.jpg" alt=""></router-link>
+                </div>
+                <div>
+                  <router-link to=""><img src="http://www.gzomick.com/statics/images/index/tj_food6.jpg" alt=""></router-link>
+                </div>
+                <div>
+                  <router-link to=""><img src="http://www.gzomick.com/statics/images/index/tj_food7.jpg" alt=""></router-link>
+                </div>
+              </div>
+            </div>
           </a-tab-pane>
           <a-tab-pane key="4" tab="咖啡饮品 /Coffee">
-            Content of Tab Pane 4
+            <div class="pane-content">
+              <div class="pane-left">
+                <router-link to="">
+                  <img src="http://www.gzomick.com/statics/images/index/kf_food1.jpg" alt="">
+                </router-link>
+              </div>
+              <div class="pane-right">
+                <div>
+                  <router-link to=""><img src="http://www.gzomick.com/images/works/kf/kf2.jpg" alt=""></router-link>
+                </div>
+                <div>
+                  <router-link to=""><img src="http://www.gzomick.com/images/works/kf/kf3.jpg" alt=""></router-link>
+                </div>
+                <div>
+                  <router-link to=""><img src="http://www.gzomick.com/images/works/kf/kf4.jpg" alt=""></router-link>
+                </div>
+                <div>
+                  <router-link to=""><img src="http://www.gzomick.com/images/works/kf/kf5.jpg" alt=""></router-link>
+                </div>
+                <div>
+                  <router-link to=""><img src="http://www.gzomick.com/images/works/kf/kf6.jpg" alt=""></router-link>
+                </div>
+                <div>
+                  <router-link to=""><img src="http://www.gzomick.com/images/works/kf/kf7.jpg" alt=""></router-link>
+                </div>
+              </div>
+            </div>
           </a-tab-pane>
         </a-tabs>
       </div>
+      <div class="works-what">
+        <router-link to="">
+          <span>想去创业学什么专业好？</span>
+        </router-link>
+      </div>
+    </div>
+    <div class="img-fix">
+      <img src="http://www.gzomick.com/statics/images/index/img-bg-icon12.png" alt="">
+    </div>
+    <div class="img-fix2">
+      <img src="http://www.gzomick.com/statics/images/index/img-bg-icon10.png" alt="">
     </div>
   </div>
 </template>
