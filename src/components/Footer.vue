a0c337936a45e4dcc2cb5b09d74d9f64db8315d1
FengZhenYu <fengzhenyu9999@163.com>
2021-03-31 16:44:27 +0800

    feat: Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="footer">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped lang="less">
+@import "src/styles/Footer";
+</style>
